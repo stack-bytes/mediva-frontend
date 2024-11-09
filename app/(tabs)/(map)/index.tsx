90608d56c26cb8957e1f7fc836c3447b17b4ba37
David-Andrei Popa <contact@popadavid.ro>
2024-11-09 23:20:53 +0200

    Added MapView

--- app/(tabs)/(map)/index.tsx
+++ app/(tabs)/(map)/index.tsx
@@ -1,13 +1,23 @@
-import { Gradient } from "@/components/images";
-import { ImageBackground } from "expo-image";
-import { SafeAreaView, View } from "react-native";
+import { View } from "react-native";
 
-import { Text } from "@/components/ui/text";
+import MapView from "react-native-maps";
+
+const INITIAL_REGION = {
+  latitude: 45.76,
+  longitude: 21.22,
+  latitudeDelta: 0.05,
+  longitudeDelta: 0.05,
+};
 
 export default function MapScreen() {
   return (
     <View className="h-full w-full bg-red-500">
-      <Text>MapScreen</Text>
+      <MapView
+        initialRegion={INITIAL_REGION}
+        showsUserLocation
+        showsMyLocationButton
+        style={{ width: "100%", height: "100%" }}
+      ></MapView>
     </View>
   );
 }
